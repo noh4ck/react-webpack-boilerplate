57194165acfd4169b0cebfe77ec964114de998a0
Jonas Rudlang <jonas.rudlang@gmail.com>
2018-02-11 16:27:40 +0100

    Remove unused UglifyJS comments

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -31,9 +31,6 @@ const config = {
       debug: false,
     }),
     new UglifyJsPlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   beautify: false,
-    // }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify('production') },
     }),
